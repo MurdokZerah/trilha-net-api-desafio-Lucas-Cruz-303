76c0b61b037f676518a439e0d1adbf64d4ffa7a8
Lucas Cruz <138832464+MurdokZerah@users.noreply.github.com>
2025-08-25 16:48:21 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,20 +4,23 @@ using TrilhaApiDesafio.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configura o DbContext com SQL Server usando a connection string do appsettings.json
 builder.Services.AddDbContext<OrganizadorContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
+// Adiciona controllers e configura serialização de enums como string
 builder.Services.AddControllers().AddJsonOptions(options =>
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Configura Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware do Swagger (ativo apenas em Development)
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -28,6 +31,8 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+// Mapeia os controllers
 app.MapControllers();
 
+// Executa a aplicação
 app.Run();
